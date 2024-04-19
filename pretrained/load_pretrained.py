bde7ca75618932144468cdc08c80f54bf38bb64b
Daniel Hocevar <danielhocevar1@gmail.com>
2024-04-19 16:06:27 -0400

    Added trained models

---
+++ pretrained/load_pretrained.py
@@ -0,0 +1,14 @@
+from models.transformer import BasicTransformer
+import torch
+
+
+def load_small_model(device):
+    model = BasicTransformer(3015, 50, 2, 500, 1, 0.0).to(device=device)
+    model.load_state_dict(torch.load("./pretrained/model_small.pt", map_location=device))
+    return model
+
+
+def load_large_model(device):
+    model = BasicTransformer(3015, 50, 5, 500, 4, 0.0).to(device=device)
+    model.load_state_dict(torch.load("./pretrained/model_large.pt", map_location=device))
+    return model
